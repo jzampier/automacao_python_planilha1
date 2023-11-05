3d1d498955e494c80be147330a30401fa4782c3c
Julio Zampier <juliozampier@gmail.com>
2023-11-04 21:31:01 -0300

    split in 2 functions prepare worksheet and send e-mail, update msg text with dynamic date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from icecream import ic
 import os
+from datetime import datetime
 import pandas as pd
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -8,53 +9,55 @@ from email import encoders
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 
-# path and list of files
-path = "bases"
-files = os.listdir(path)
 
 # Create consolidated worksheet
-consolidated_worksheet = pd.DataFrame()
+def create_worksheet(folder: str) -> str:
+    # path and list of files
+    files = os.listdir(folder)
+    # Create an empty dataframe
+    consolidated_worksheet = pd.DataFrame()
+    # Loop through files adding sales data to the dataframe
+    for file_name in files:
+        full_path = os.path.join(folder, file_name)
+        try:
+            sales_table = pd.read_csv(full_path)
+            consolidated_worksheet = pd.concat([consolidated_worksheet, sales_table])
+        except Exception as e:
+            ic(f"Error reading file {full_path}: {e}")
+    # Sort the dataframe
+    consolidated_worksheet = consolidated_worksheet.sort_values(by='first_name')
+    consolidated_worksheet = consolidated_worksheet.reset_index(drop=True)
+    ic(consolidated_worksheet)
 
-# Loop through files
-for file_name in files:
-    full_path = os.path.join(path, file_name)
-    try:
-        sales_table = pd.read_csv(full_path)
-        consolidated_worksheet = pd.concat([consolidated_worksheet, sales_table])
-    except Exception as e:
-        ic(f"Error reading file {full_path}: {e}")
+    # Save consolidated worksheet in an Excel file
+    consolidated_worksheet.to_excel('Sales.xlsx', index=False)
+    return 'Sales.xlsx'
 
-consolidated_worksheet = consolidated_worksheet.sort_values(by='first_name')
-consolidated_worksheet = consolidated_worksheet.reset_index(drop=True)
-ic(consolidated_worksheet)
 
-# Save consolidated worksheet in an Excel file
-consolidated_worksheet.to_excel('Sales.xlsx', index=False)
-excel_file: str = 'Sales.xlsx'
 # Email routine
-load_dotenv()
-# environment variables
-SENDER = os.getenv('SENDER')
-PASSWORD = os.getenv('PASSWORD')
-RECEIVER = os.getenv('RECEIVER')
-
-
 def send_email(attachment_file: str):
+    # environment variables
+    load_dotenv()
+    SENDER = os.getenv('SENDER')
+    PASSWORD = os.getenv('PASSWORD')
+    RECEIVER = os.getenv('RECEIVER')
     # Prepare the email (message + attachment)
     msg = MIMEMultipart()
     msg['From'] = SENDER
     msg['To'] = RECEIVER
-    msg['Subject'] = 'Sales Report'
-    body = """<p><b>Relatório de vendas</b></p>
+    today_date: str = datetime.today().strftime('%d/%m/%Y')
+    msg['Subject'] = f'Relatório de vendas - {today_date}'
+    body = f"""<h3>Relatório de vendas do dia {today_date}</h3>
                     <p>Prezado(a) gerente do setor de vendas.</p>
                     <p></p>
                     <p></p>
-                    <p>Segue anexado o relatório diário de vendas com os dados atualizados.</p>
+                    <p>Segue anexado o relatório diário de vendas com os dados atualizados na 
+                    data de hoje ({today_date}).</p>
                     <p>Caso tenha algum problema, favor nos avisar.</p>
                     <p></p>
                     <p></p>
                     <p>Cordialmente,</p>
-                    <p>Análise de dados</p>"""
+                    <p>Equipe de análise de dados</p>"""
     msg.attach(MIMEText(body, 'html'))
     file = attachment_file
     try:
@@ -80,4 +83,5 @@ def send_email(attachment_file: str):
         ic(f"Error sending email: {e}")
 
 
+excel_file: str = create_worksheet('bases')
 send_email(excel_file)
